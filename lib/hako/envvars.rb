b7cbe5df36b211befe8f05e32bc3a8358c448275
Jun Fukaya <fukajun.shark@gmail.com>
2020-10-28 18:28:30 +0900

    bundle gem

---
+++ lib/hako/envvars.rb
@@ -0,0 +1,8 @@
+require "hako/envvars/version"
+
+module Hako
+  module Envvars
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/hako/envvars/version.rb
@@ -0,0 +1,5 @@
+module Hako
+  module Envvars
+    VERSION = "0.1.0"
+  end
+end
