28ab225315fcf1219e18513fd5ae6d6c06b8b02d
Jun Fukaya <fukajun.shark@gmail.com>
2020-10-29 01:32:20 +0900

    Remove raise_error option

--- lib/hako/env_providers/envvars.rb
+++ lib/hako/env_providers/envvars.rb
@@ -5,17 +5,13 @@ require 'hako/env_provider'
 module Hako
   module EnvProviders
     class Envvars < EnvProvider
-      def initialize(_root_path, options)
-        merged_options = default_options.merge(options)
-        @raise_error = merged_options['raise_error']
+      def initialize(_root_path, _options)
       end
 
       def ask(variables)
         env = {}
         variables.each do |key|
-          if valid_key?(key)
-            env[key] = ENV[key]
-          end
+          env[key] = ENV[key]
         end
         env
       end
@@ -27,20 +23,12 @@ module Hako
       def ask_keys(variables)
         keys = []
         variables.each do |key, _|
-          keys << valid_key?(key)
+          keys << ENV.has_key?(key)
         end
         keys
       end
 
       private
-      def valid_key?(key)
-        raise_error? ? ENV.has_key?(key) : true
-      end
-
-      def raise_error?
-        @raise_error
-      end
-
       def default_options
         { 'raise_error' => true }
       end
